6781d6c35278c8830003435eb0b314119617f53f
Mercury <mercury.me@qq.com>
2026-07-30 21:37:59 +0800

    update version to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.mercury.sonypods"
         minSdk = 35
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
         buildConfigField("long", "BUILD_TIMESTAMP", System.currentTimeMillis().toString())
     }
 
