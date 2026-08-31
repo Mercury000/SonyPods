fc7517614109efd70946510c62e8b32d6653f9d6
Mercury <mercury.me@qq.com>
2026-08-31 17:41:24 +0800

    chore:消除编译警告

--- app/src/main/java/dev/sonypods/ui/pages/PodDetailPage.kt
+++ app/src/main/java/dev/sonypods/ui/pages/PodDetailPage.kt
@@ -44,7 +44,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
